5ad4709b4b76044e5804b5b3ebdc0e6398cf2469
Blindspot22 <enowscott53@gmail.com>
2025-04-26 15:51:25 +0100

    fix: removed target

--- backend/target/debug/deps/made_in_cameroon_market-712d1bdd76df7725.d
+++
@@ -1,5 +0,0 @@
-/home/scott/ai/made-in-cameroon-market/backend/target/debug/deps/made_in_cameroon_market-712d1bdd76df7725: crates/api/src/main.rs
-
-/home/scott/ai/made-in-cameroon-market/backend/target/debug/deps/made_in_cameroon_market-712d1bdd76df7725.d: crates/api/src/main.rs
-
-crates/api/src/main.rs:
--- backend/target/debug/made-in-cameroon-market.d
+++
@@ -1,1 +0,0 @@
-/home/scott/ai/made-in-cameroon-market/backend/target/debug/made-in-cameroon-market: /home/scott/ai/made-in-cameroon-market/backend/crates/api/src/main.rs
